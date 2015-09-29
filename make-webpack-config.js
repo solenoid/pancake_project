e7255001d7527932244b618e0ca465eba1a4df58
Erik Solen <esolen@yieldbot.com>
2015-09-29 10:55:57 -0400

    bump to most recent packages except those that are held back on purpose

--- make-webpack-config.js
+++ make-webpack-config.js
@@ -5,6 +5,7 @@ module.exports = function (options) {
     resolve: {
       alias: {
         // Workaround https://github.com/Reactive-Extensions/RxJS/issues/832, until it's fixed
+        // Also keep the rx we install pinned to the supported falcor version vs. the latest
         'rx$': 'rx/dist/rx'
       }
     },
